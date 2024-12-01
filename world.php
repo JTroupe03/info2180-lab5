c1c486b7a6ef993662ea0edd23e62f9b59f7c694
JTroupe03 <jostroupe2@gmail.com>
2024-12-01 17:43:05 -0500

    Finished Activity 3

--- world.php
+++ world.php
@@ -1,13 +1,18 @@
 <?php
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: text/plain");
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE");
+
 $host = 'localhost';
 $username = 'lab5_user';
-$password = '';
+$password = 'password123';
 $dbname = 'world';
 
 $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
 $stmt = $conn->query("SELECT * FROM countries");
-
-$results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+$country = filter_input(INPUT_GET,"query", FILTER_SANITIZE_STRING);
+$country_1 = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%'");
+$results = $country_1->fetchAll(PDO::FETCH_ASSOC);
 
 ?>
 <ul>
